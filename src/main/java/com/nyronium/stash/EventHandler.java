1121b69a93c9d26e8b820b2b0c329c1fe03abd79
NoahOnFyre <noahonfyre@gmail.com>
2025-06-04 10:58:22 +0200

    feat: add core functionality

--- src/main/java/com/nyronium/stash/EventHandler.java
+++ src/main/java/com/nyronium/stash/EventHandler.java
@@ -1,4 +1,13 @@
 package com.nyronium.stash;
 
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Stash.ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler {
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+        StashCommand.register(event.getDispatcher());
+    }
 }
--- src/main/java/com/nyronium/stash/StashCommand.java
+++ src/main/java/com/nyronium/stash/StashCommand.java
@@ -1,4 +1,39 @@
 package com.nyronium.stash;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ChestMenu;
+import org.jetbrains.annotations.NotNull;
+
 public class StashCommand {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("stash")
+                .executes(context -> {
+                    ServerPlayer player = context.getSource().getPlayerOrException();
+
+                    Component title = Component.literal("Public Stash");
+
+                    player.openMenu(new MenuProvider() {
+                        @Override
+                        public @NotNull Component getDisplayName() {
+                            return title;
+                        }
+
+                        @Override
+                        public AbstractContainerMenu createMenu(int id, @NotNull Inventory playerInventory, @NotNull Player player) {
+                            return ChestMenu.sixRows(id, playerInventory, Stash.container);
+                        }
+                    });
+
+                    return Command.SINGLE_SUCCESS;
+                }));
+    }
 }
